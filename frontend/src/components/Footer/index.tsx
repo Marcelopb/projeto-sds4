632d3d241b414bfbd48bcf9fa3558090bd79c747
Marceloap <marceloa570@gmail.com>
2021-09-09 10:07:47 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/Marcelopb/projeto-sds4" target="_blank" rel="noreferrer">Marcelo Auguto</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://www.linkedin.com/in/marcelo-augusto-barreiro-1b13b721/" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+
+    );
+}
+
+export default Footer;
